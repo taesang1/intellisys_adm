ab5b4ee7daa1d65d8894d74a5a12236be69594cc
taesang <tsjung@intellisys.co.kr>
2022-06-23 18:15:08 +0900

    헤더 ui 완료

---
+++ intellisys/src/layout/layout_header.vue
@@ -0,0 +1,12 @@
+<template>
+  <b-container class="bv-example-row" style="max-width: 100%">
+    <b-row style="background-color: #f9f2f2; padding: 5px">
+      <b-col>IntelliSys ADMIN</b-col>
+      <b-col style="text-align: right">로그아웃</b-col>
+    </b-row>
+
+    <b-row style="padding: 5px; border-bottom: 1px solid #eceeef">
+      <b-col>&nbsp;</b-col>
+    </b-row>
+  </b-container>
+</template>
