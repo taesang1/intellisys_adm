8dd21dbeea5d186576f0b4a0b3d0ea6bb4d593a8
taesang <tsjung@intellisys.co.kr>
2022-06-23 18:14:52 +0900

    왼쪽 메뉴 ui 완료

---
+++ intellisys/src/layout/layout_meun.vue
@@ -0,0 +1,57 @@
+<template>
+    <div style="height: 100%; border-right: 1px solid #eceeef; padding-left: 10px;">
+        <li>
+            <a>Service메뉴 관리</a>
+            <ul>
+                <li>
+                    <a>조회</a>
+                </li>
+                <li>
+                    <a>등록</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a>Service 적용사례 관리</a>
+            <ul>
+                <li>
+                    <a>조회</a>
+                </li>
+                <li>
+                    <a>등록</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a>문의하기 관리</a>
+            <ul>
+                <li>
+                    <a>조회</a>
+                </li>
+                <li>
+                    <a>폼 관리</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a>blog 관리</a>
+            <ul>
+                <li>
+                    <a>조회</a>
+                </li>
+                <li>
+                    <a>작성</a>
+                </li>
+            </ul>
+        </li>
+    </div>
+</template>
+<style>
+    li{
+        font-size: 15px;
+        padding: 5px 0px;
+    }
+    ul, li{
+        list-style:none;
+    }
+</style>
