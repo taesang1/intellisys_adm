c5ac5d813999cfadb8ddeb2dcd00766b069893e5
taesang <tsjung@intellisys.co.kr>
2022-06-27 15:32:49 +0900

    라우터 기능 추가

--- intellisys/src/App.vue
+++ intellisys/src/App.vue
@@ -8,8 +8,9 @@
           <layout_meun />
         </b-col>
         <b-col style="height: 100%">
-          <h2 style="padding: 10px">Blog 목록 조회</h2>
-          <b-table striped hover :items="items" class=""></b-table>
+          <!-- https://bootstrap-vue.org/docs/components/table#table -->
+          <router-view></router-view>
+          <!-- <ad/> -->
         </b-col>
       </b-row>
     </b-container>
@@ -20,31 +21,12 @@
 import layout_header from "./layout/layout_header.vue";
 import layout_meun from "./layout/layout_meun.vue";
 
-import axios from "axios";
-
 export default {
+  name : 'App',
   components: {
     layout_header,
     layout_meun,
-  },
-  data: function () {
-    return {
-      blog_list: [],
-      items: [
-          { No: 76, 제목: "[ET시론]디지털 전환을 바라는 C레벨에게",설명: "디지털 전환(DT:Digital Transformation)이 기업의 최대 화두다. 얼마 전까지 디지털 전환은 기업 미래전략의 상위 현안 중 하나 정도로 겨우 여겨졌는데, 코로나 사태 이후에는 생존을 위한 필수 전략 과제로 올라섰다. 새 정부도 디지털 플랫폼...",일자: "2022-04-16"},
-          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-          { age: 38, first_name: 'Jami', last_name: 'Carney' }
-        ]
-    };
-  },
-  created: function () {
-    axios
-      .get("http://127.0.0.1:8000/blog")
-      .then((Response) => console.log((this.blog_list = Response.data[0])))
-      .catch((Error) => {
-        console.log(Error);
-      });
-  },
+  }
 };
+
 </script>
