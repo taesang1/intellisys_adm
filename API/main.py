4cff57faaf4ce090797a654ef4013d6831bfc768
taesang <tsjung@intellisys.co.kr>
2022-06-27 15:32:09 +0900

    블로그 내림차순으로 받아오도록 수정

--- API/main.py
+++ API/main.py
@@ -31,7 +31,7 @@ async def shutdown():
 
 @app.get("/blog", response_description="blog 목록 가져오기")
 async def get_codis():
-    db.cur.execute('SELECT serial,title,description,publishedAt FROM intellisys1.press;', args={})
+    db.cur.execute('SELECT serial,title,description,publishedAt FROM intellisys1.press ORDER BY publishedAt desc;')
     rows = [data for data in db.cur.fetchall()]
     return rows
      
