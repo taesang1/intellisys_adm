8ccaf60b4c4f05a61e8b0c1f7103cc9dd21de214
taesang <tsjung@intellisys.co.kr>
2022-06-27 15:33:23 +0900

    라우터 기능 설정

--- intellisys/src/main.js
+++ intellisys/src/main.js
@@ -1,7 +1,8 @@
 import App from './App.vue'
 import {createApp} from 'vue'
 import BootstrapVue3 from 'bootstrap-vue-3'
-
+import Router from './router'
+// import Vue from 'vue'
 // Optional, since every component import their Bootstrap functionality
 // the following line is not necessary
 // import 'bootstrap'
@@ -11,5 +12,10 @@ import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 const app = createApp(App)
 app.use(BootstrapVue3)
+app.use(Router)
 app.mount('#app')
+
+// Vue.config.productionTip = false
+
+
 
